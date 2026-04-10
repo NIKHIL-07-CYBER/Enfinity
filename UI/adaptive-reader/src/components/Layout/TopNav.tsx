d1e585b418c78cf5d0a7d5cb184b9cad22a7fe2d
VrajVaghela <vrjvghl@gmail.com>
2026-04-11 00:45:13 +0530

    changed name from enfinity to briefly

--- UI/adaptive-reader/src/components/Layout/TopNav.tsx
+++ UI/adaptive-reader/src/components/Layout/TopNav.tsx
@@ -85,7 +85,7 @@ export const TopNav: React.FC = () => {
         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5">
           <path d="M12 12c-2-2.67-4-4-6-4a4 4 0 1 0 0 8c2 0 4-1.33 6-4Zm0 0c2 2.67 4 4 6 4a4 4 0 1 0 0-8c-2 0-4 1.33-6 4Z" />
         </svg>
-        enfinity
+        briefly
       </button>
 
       {/* Nav links */}
